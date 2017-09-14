afdee5a89632505dba38cd0b2907116b6ca47642
thisisaaronland <whosonfirst@mapzen.com>
2017-09-14 12:49:26 +0000

    start stubbing out process flags

--- flags/process.go
+++ flags/process.go
@@ -1,3 +1,31 @@
 package flags
 
-// please write me...
+import (
+	"github.com/whosonfirst/go-whosonfirst-updated/process"
+	"strconv"
+	"strings"
+)
+
+type ProcessFlags struct {
+	flags []string
+}
+
+func (fl *ProcessFlags) String() string {
+	return strings.Join(fl.flags, "\n")
+}
+
+func (fl *ProcessFlags) Set(value string) error {
+	fl.flags = append(fl.flags, value)
+	return nil
+}
+
+func (fl ProcessFlags) ToProcesses() ([]process.Process, error) {
+
+	procs := make([]process.Process, 0)
+
+	for _, p := range fl.flags {
+
+	}
+
+	return procs, nil
+}
